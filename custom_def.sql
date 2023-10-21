0b767433d89c2c08120b8921e9e4fee9f7b24a74
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-10-21 19:19:33 +0530

    creating custom function adding few more custom functions

--- custom_def.sql
+++ custom_def.sql
@@ -1,11 +1,20 @@
-CREATE FUNCTION AddNumbers (
-    @num1 INT,
-    @num2 INT
-)
-RETURNS INT
-AS
-BEGIN
-    DECLARE @result INT;
-    SET @result = @num1 + @num2;
-    RETURN @result;
-END;
+USE employeemanagement;
+-- Finding all the available projects
+ select distinct pnumber from projects;
+
+
+--Create the function for finding stack required for the project
+ SELECT s.stack, pr.pnumber
+ FROM stacks s
+ INNER JOIN projectrequirements pr ON s.Id = pr.stackId;
+ WHERE pr.pnumber = 2;
+
+
+
+-- Finding employee matching skills required for the project
+select pr.stackId,sk.employeeSsn
+from skills as sk
+right join projectrequirements as pr on pr.stackId=sk.stackId
+where pr.pnumber =2;
+
+
