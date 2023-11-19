8ab1aa3efd3e4aeb3ad05afd52a8d793b492ca08
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-11-19 15:40:39 +0530

    adding project html

--- run.py
+++ run.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,flash,redirect,url_for
 from forms import addEmployeeForm, addProjectForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ba3090250e17faed336ea51e083266e0'
@@ -7,10 +7,23 @@ app.config['SECRET_KEY'] = 'ba3090250e17faed336ea51e083266e0'
 @app.route("/home")
 def home():
     return render_template('home.html')
-@app.route("/add_employees")
+@app.route("/add_employees",methods=['POST','GET'])
 def add_employees():
     form=addEmployeeForm()
+    if form.validate_on_submit():
+        flash(f'Welcome Aboard {form.fname.data.upper()}!','success')
+        return redirect(url_for('home'))
     return render_template('add_employee.html',title='add employee',form=form)
+
+
+@app.route("/add_projects",methods=['POST','GET'])
+def add_projects():
+    form=addProjectForm()
+    if form.validate_on_submit():
+        flash(f'Work Alert : {form.pname.data.upper()}!','success')
+        return redirect(url_for('home'))
+    return render_template('add_project.html', title='add project', form=form)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
