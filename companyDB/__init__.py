5206bf2376cfcad7f3d7a7e408c0c4e02e424bbd
Vaishakh Raveendran <vaishakhraveendran2002@gmail.com>
2023-11-30 23:29:37 +0530

    final project

--- companyDB/__init__.py
+++ companyDB/__init__.py
@@ -4,7 +4,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'ba3090250e17faed336ea51e083266e0'
-app.config['SQLALCHEMY_DATABASE_URI'] ='sqlite:///site.db'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql://root:l%40xmi2421@localhost/employeemanagement'
 db = SQLAlchemy(app)
 
 from companyDB import routes
--- companyDB/routes.py
+++ companyDB/routes.py
@@ -6,22 +6,23 @@ from companyDB import app,db
 @app.route("/")
 @app.route("/home")
 def home():
-    return render_template('home.html')
+    employees = Employee.query.all()
+    return render_template('home.html',employees=employees)
 @app.route("/add_employees",methods=['POST','GET'])
 def add_employees():
     form=addEmployeeForm()
     if form.validate_on_submit():
         employee_instance = Employee(
-            fname=form_instance.fname.data,
-            minit=form_instance.minit.data,
-            lname=form_instance.lname.data,
-            ssn=form_instance.ssn.data,
-            bdate=form_instance.bdate.data,
-            address=form_instance.address.data,
-            sex=form_instance.sex.data,
-            salary=form_instance.salary.data,
-            superSsn=form_instance.superSsn.data,
-            dno=form_instance.dno.data
+            fname=form.fname.data,
+            minit=form.minit.data,
+            lname=form.lname.data,
+            ssn=form.ssn.data,
+            bdate=form.bdate.data,
+            address=form.address.data,
+            sex=form.sex.data,
+            salary=form.salary.data,
+            superSsn=form.super_ssn.data,
+            dno=form.dno.data
         )
         db.session.add(employee_instance)
         db.session.commit()
@@ -35,10 +36,10 @@ def add_projects():
     form=addProjectForm()
     if form.validate_on_submit():
         project_instance = Project(
-            pname=form_instance.pname.data,
-            pnumber=form_instance.pnumber.data,
-            plocation=form_instance.plocation.data,
-            dnum=form_instance.dnum.data
+            pname=form.pname.data,
+            pnumber=form.pnumber.data,
+            plocation=form.plocation.data,
+            dnum=form.dnum.data
         )
         db.session.add(project_instance)
         db.session.commit()
